6eae08099d67c788761376c7edee964b5c60b2f8
Shin <swtran21@gmail.com>
2019-01-27 20:09:15 -0800

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+import re
+import traceback
+
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -38,35 +41,94 @@ To submit your homework:
   * Commit and push your changes to your fork.
   * Submit a link to your Session03 fork repository!
 
-
 """
+
+
+def home():
+    page = """
+<h1>WSGI Calculator</h1>
+<h2>This website will help you solve basic math problems given two integers.</h2>
+<html>
+Here's how to use this page:</br>
+    http://localhost:8080/add/23/42 => 65</br>
+    http://localhost:8080/subtract/23/42 => -19</br>
+    http://localhost:8080/multiply/3/5 => 15</br>
+    http://localhost:8080/divide/22/11 => 2</br>
+</html>
+"""
+    return page
 
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
-
     # TODO: Fill sum with the correct value, based on the
     # args provided.
-    sum = "0"
+    v1 = args[0]
+    v2 = args[1]
+    try:
+        the_sum = int(v1) + int(v2)
+    except ValueError:
+        return "Invalid values entered, need to enter integers"
+    return str(the_sum)
 
-    return sum
 
-# TODO: Add functions for handling more arithmetic operations.
+def subtract(*args):
+    """ Returns a STRING with the difference of the arguments """
+    v1 = args[0]
+    v2 = args[1]
+    try:
+        the_diff = int(v1) - int(v2)
+    except ValueError:
+        return "Invalid values entered, need to enter integers"
+    return str(the_diff)
+
+
+def multiply(*args):
+    """ Returns a STRING with the product of the arguments """
+    v1 = args[0]
+    v2 = args[1]
+    try:
+        the_prod = int(v1) * int(v2)
+    except ValueError:
+        return "Invalid values entered, need to enter integers"
+    return str(the_prod)
+
+
+def divide(*args):
+    """ Returns a STRING with the quotient of the arguments """
+    v1 = args[0]
+    v2 = args[1]
+    try:
+        the_quot = int(v1) / int(v2)
+    except ValueError:
+        return "Invalid values entered, need to enter integers"
+    return str(the_quot)
+
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
-
     # TODO: Provide correct values for func and args. The
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
-
+    funcs = {'':home,
+        'add':add,
+        'subtract':subtract,
+        'multiply':multiply,
+        'divide':divide
+    }
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
     return func, args
+
 
 def application(environ, start_response):
     # TODO: Your application code from the book database
@@ -76,9 +138,30 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
